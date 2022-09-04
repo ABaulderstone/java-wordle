a3d1081b244976ce6775a57d2f6f10929f1c3b67
Abaulderstone <alex.baulderstone@coderacademy.edu.au>
2022-09-05 01:15:26 +1000

    guess class first pass

---
+++ src/main/java/gamelogic/Guess.java
@@ -0,0 +1,64 @@
+package gamelogic;
+
+import com.example.TextColor;
+
+public class Guess {
+  String word;
+  String colorizedOutput;
+  char[] letters;
+  char[] greenLetters;
+  char[] yellowLetters;
+  
+  public Guess(String word) {
+    this.word = word;
+    this.letters = word.toCharArray();
+    this.greenLetters = new char[5];
+  }
+
+  public boolean compare(String selectedWord) {
+    extractGreenLetters(selectedWord);
+    extractYellowLetters(selectedWord);
+    return word.equals(selectedWord);
+  }
+
+  public void extractGreenLetters(String selectedWord) {
+    char[] selectedLetters = selectedWord.toCharArray();
+    for(byte i = 0; i < selectedLetters.length; i++) {
+        if(selectedLetters[i] == letters[i]) {
+          greenLetters[i] = selectedLetters[i];
+          letters[i] = '*';
+        }
+    }
+  }
+
+  public void extractYellowLetters(String selectedWord) {
+    char[] selectedLetters = selectedWord.toCharArray();
+    for(byte i = 0; i < selectedLetters.length; i++) {
+        String remainingLetters = new String(letters);
+        String subString = String.valueOf(selectedLetters[i]);
+        if(remainingLetters.contains(subString)) {
+          yellowLetters[i] = selectedLetters[i];
+          letters[i] = '*';
+        }
+    }
+  }
+
+  public String generateColoredString(){ 
+    StringBuilder builder = new StringBuilder();
+    for(byte i = 0; i < 5; i++) {
+      char currentChar = word.charAt(i);
+       if( greenLetters[i] == currentChar) {
+         builder.append(TextColor.GREEN.text + currentChar + TextColor.RESET.text);
+         continue;
+       }
+
+       if(yellowLetters[i] == currentChar) {
+         builder.append(TextColor.YELLOW.text + currentChar + TextColor.RESET.text);
+        continue;
+       }
+          builder.append(currentChar);
+      }
+      return builder.toString();
+    }
+  }
+
